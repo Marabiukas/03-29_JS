185cfde07b8fb82bb6aa7f379396c608100361f6
Marabiukas <mariusvienoj@gmail.com>
2022-03-29 13:50:10 +0300

    Variables

---
+++ js/variables.js
@@ -0,0 +1,46 @@
+/*
+KINTAMIEJI
+
+Iniciavimo budas:
+const - default iniciavimo budas
+let - antrinis budas inicijuoti, kai zinome, jog reiskme tures keistis
+var - pats seniausias ir geriau niekada nenaudoti, jei rupi saugumas
+
+Duomenu tipas: 
+- number (skaicius)
+- string (tekstas)
+- boolean (logone reiskme: true arba false)
+- array (masyvas, sarasas, matrica*)
+*/
+
+const luckyNumber = 7;
+console.log(luckyNumber);
+
+let pinigine = 0;
+console.log(pinigine);
+
+pinigine = 5;
+console.log(pinigine);
+
+console.log('1) laba diena, Petras');
+
+const vardas = 'Petras';
+console.log(vardas)
+console.log('2) laba diena, ' + vardas + '.')
+
+var nieko = 'nieko'
+console.log(nieko)
+
+const arVedes = true;
+const arTuriVaiku = false;
+
+console.log('Ar vedes?', arVedes)
+
+console.log(2+2*2)
+console.log('2+2*2')
+
+const pazymiai = [10, 2, 8, 4, 6];
+console.log('Pazymiai:', pazymiai);
+
+const studentai = ['Petras', 'Maryte', 'Jonas', 'Ona'];
+console.log(studentai);
