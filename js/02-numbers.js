2af331d1df0568a5ef7fb399cd432484d6937910
Marabiukas <mariusvienoj@gmail.com>
2022-03-30 10:37:05 +0300

    Numbers

---
+++ js/02-numbers.js
@@ -0,0 +1,52 @@
+/*
+NUMBERS:
+- normalus:
+    - sveikieji
+    - desimtainiai
+- nenormalus:
+    - NaN (not-a-number)
+    - Infinity; -Infinity
+*/
+
+const sveikasis = 5;
+const desimtainis = 3.14;
+const neigiamasDesimtainis = -3.14
+const neSkaicius = NaN;
+const begalybe = Infinity;
+
+const pirmas = 7;
+const antras = 5;
+
+const dalmuo = pirmas / antras;
+console.log(dalmuo);
+
+console.log('----------------')
+console.log(7 + 5);
+console.log(7 - 5);
+console.log(7 * 5);
+console.log(7 / 5);
+
+console.log('----------------')
+console.log(7 + '5');
+console.log(7 - '5');
+console.log(7 * '5');
+console.log(7 / '5');
+
+console.log('num, string')
+console.log('7' + '5');
+console.log('7' - '5');
+console.log('7' * '5');
+console.log('7' / '5');
+
+console.log('num; true')
+console.log(7 + true);
+console.log(7 - true);
+console.log(7 * true);
+console.log(7 / true);
+
+console.log('num; false')
+console.log(7 + false);
+console.log(7 - false);
+console.log(7 * false);
+console.log(7 / false);
+
